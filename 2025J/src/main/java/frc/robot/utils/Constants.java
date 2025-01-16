a57f9a69f5b1d7ab58c164a3536e77e37a9b52b9
philiphan0109 <philiphan0109@hotmail.com>
2025-01-16 15:37:35 -0500

    Implemented drivetrain

--- 2025J/src/main/java/frc/robot/utils/Constants.java
+++ 2025J/src/main/java/frc/robot/utils/Constants.java
@@ -1,5 +1,87 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.utils;
 
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants
+ * should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the
+ * constants are needed, to reduce verbosity.
+ */
 public final class Constants {
-    
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static class ModuleConstants {
+    public static final double kDriveMotorCurrentLimit = 40;
+    public static final double kSteerMotorCurrentLimit = 40;
+    public static final double kWheelDiameterInches = 4.0;
+
+    public static final double kDriveMotorReduction = 6.12;
+    public static final double kDriveEncoderPositionFactor = (Math.PI * Units.inchesToMeters(kWheelDiameterInches))
+        / kDriveMotorReduction;
+    public static final double kDriveEncoderVelocityFactor = (Math.PI * Units.inchesToMeters(kWheelDiameterInches))
+        / kDriveMotorReduction;
+
+    // TODO: Update Real Values
+    public static final double kSteerMotorReduction = 0.0;
+
+    public static final double kDriveS = 0.0;
+    public static final double kDriveV = 0.0;
+    public static final double kDriveA = 0.0;
+    public static final double kDriveP = 0.0;
+    public static final double kDriveI = 0.0;
+    public static final double kDriveD = 0.0;
+    public static final double kDriveFF = 0.0;
+
+    public static final double kSteerS = 0.0;
+    public static final double kSteerV = 0.0;
+    public static final double kSteerA = 0.0;
+    public static final double kSteerP = 0.0;
+    public static final double kSteerI = 0.0;
+    public static final double kSteerD = 0.0;
+    public static final double kSteerFF = 0.0;
+
+  }
+
+  public static class DriveConstants {
+    // TODO: Update Real Values
+    public static final double kTrackWidth = 0.0;
+    public static final double kWheelBase = 0.0;
+
+    public static final double kMaxFloorSpeed = 0.0;
+
+    public static final Translation2d[] swerveModuleLocations = {
+        new Translation2d(kWheelBase / 2.0, kTrackWidth / 2.0),
+        new Translation2d(kWheelBase / 2.0, -kTrackWidth / 2.0),
+        new Translation2d(-kWheelBase / 2.0, kTrackWidth / 2.0),
+        new Translation2d(-kWheelBase / 2.0, -kTrackWidth / 2.0),
+    };
+
+    public static final SwerveDriveKinematics kinematics = new SwerveDriveKinematics(
+        swerveModuleLocations[0],
+        swerveModuleLocations[1],
+        swerveModuleLocations[2],
+        swerveModuleLocations[3]);
+
+    // TODO: Update Real Values
+    public static final double kFrontLeftCancoderOffset = 0.0;
+    public static final double kFrontRightCancoderOffset = 0.0;
+    public static final double kBackLeftCancoderOffset = 0.0;
+    public static final double kBackRightCancoderOffset = 0.0;
+
+  }
 }
--- 2025J/src/main/java/frc/robot/utils/RobotMap.java
+++ 2025J/src/main/java/frc/robot/utils/RobotMap.java
@@ -1,5 +1,24 @@
 package frc.robot.utils;
 
-public final class RobotMap {
-    
+public class RobotMap {
+    // TODO: update with real values
+    public static final String CANIVORE_NAME = "";
+    public static final int GYRO_ID = 0;
+
+    public static final int FRONT_LEFT_MODULE_DRIVE_ID = 0;
+    public static final int FRONT_RIGHT_MODULE_DRIVE_ID = 0;
+    public static final int BACK_LEFT_MODULE_DRIVE_ID = 0;
+    public static final int BACK_RIGHT_MODULE_DRIVE_ID = 0;
+
+    public static final int FRONT_LEFT_MODULE_TURN_ID = 0;
+    public static final int FRONT_RIGHT_MODULE_TURN_ID = 0;
+    public static final int BACK_LEFT_MODULE_TURN_ID = 0;
+    public static final int BACK_RIGHT_MODULE_TURN_ID = 0;
+
+    public static final int FRONT_LEFT_MODULE_CANCODER_ID = 0;
+    public static final int FRONT_RIGHT_MODULE_CANCODER_ID = 0;
+    public static final int BACK_LEFT_MODULE_CANCODER_ID = 0;
+    public static final int BACK_RIGHT_MODULE_CANCODER_ID = 0;
+
+ 
 }
