da64c8b1040f648e88b116a83aeec5e0f7dec2f4
Megan Cheng <mcheng-27@peddie.org>
2025-02-07 17:21:28 -0500

    add colors

--- 2025J/src/main/java/frc/robot/subsystems/Lights.java
+++ 2025J/src/main/java/frc/robot/subsystems/Lights.java
@@ -1,24 +1,29 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.led.CANdle;
+import com.ctre.phoenix.led.ColorFlowAnimation.Direction;
+import com.ctre.phoenix.led.ColorFlowAnimation;
 import com.ctre.phoenix.led.RainbowAnimation;
+import com.ctre.phoenix.led.StrobeAnimation;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.utils.LimelightHelpers;
 
 
 
 public class Lights extends SubsystemBase{
     public static Lights lights;
     private final CANdle candle;
-    private double lastIntook;
+    private double lastIntaked;
     private boolean isClimbing;
 
     public enum LightState{
         IDLE,
         ALGAE_INTAKING,
         CORAL_INTAKING,
-        INTOOK,
+        INTAKEN,
         CLIMBING,
         DONE_CLIMBING,
         HAS_TARGET,
@@ -45,8 +50,8 @@ public class Lights extends SubsystemBase{
         candle.setLEDs(0,0,0);
 
         switch (request){
-            case INTOOK:
-                lastIntook = Timer.getFPGATimestamp();
+            case INTAKEN:
+                lastIntaked = Timer.getFPGATimestamp();
                 break;
             case HAS_TARGET:
                 candle.setLEDs(0, 0, 255);
@@ -71,12 +76,23 @@ public class Lights extends SubsystemBase{
                 candle.setLEDs(0, 128, 128);
                 break;
             case CORAL_INTAKING:
-                candle.setLEDs(255, 255, 255);
+                candle.setLEDs(255, 127, 80); //spsd to be a coral color
                 break;
-            case INTOOK:
-                lastIntook = Timer.getFPGATimestamp();
+            case INTAKEN:
+                candle.animate(new StrobeAnimation(0, 255, 0, 0, 0.3, 8), 0);
+                if (!DriverStation.isAutonomous()){
+                    LimelightHelpers.setLEDMode_ForceBlink("limelight-left"); //change based on limelight names
+                    LimelightHelpers.setLEDMode_ForceBlink("limelight-right");
+                    LimelightHelpers.setLEDMode_ForceBlink("limelight-back");
+                }
+
+                if(Timer.getFPGATimestamp() - lastIntaked > 1.5){
+                    //TODO: verify that there is no additional limelight logic needed here
+                    requestState(LightState.IDLE);
+                }
                 break;
             case CLIMBING:
+                candle.animate(new ColorFlowAnimation(255, 0, 255, 0, 0.3, 8, Direction.Forward), 0);
                 break;
             case DONE_CLIMBING:
                 candle.animate(new RainbowAnimation(),0);
