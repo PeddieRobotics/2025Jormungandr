02732bc66e4097d3d06e432c7817d1d9213e9785
kaseyLee123 <142427883+kaseyLee123@users.noreply.github.com>
2025-02-28 18:39:53 -0500

    Update Constants.java

--- 2025J/src/main/java/frc/robot/utils/Constants.java
+++ 2025J/src/main/java/frc/robot/utils/Constants.java
@@ -230,7 +230,8 @@ public final class Constants {
         public static final double kArmForwardTorqueCurrentLimit = 40;
         public static final double kArmReverseTorqueCurrentLimit = -40;
 
-        public static final double kArmMagnetOffset = -0.652588 * 2.0 + 0.5;
+        // public static final double kArmMagnetOffset = -0.652588 * 2.0 + 0.5;
+        public static final double kArmMagnetOffset = -0.686768 * 2.0 + 0.5;
 
         public static final double kArmReduction = 120.0 / 1.0;
         public static final double kArmRotorToSensorRatio = kArmReduction / 2.0;
