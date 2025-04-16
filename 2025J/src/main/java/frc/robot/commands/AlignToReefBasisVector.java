6018aa9a5bbb737d53cd48a0725ab4376b2ca219
Olivia <148790950+timmoli4350@users.noreply.github.com>
2025-04-16 11:14:39 -0500

    a

--- 2025J/src/main/java/frc/robot/commands/AlignToReefBasisVector.java
+++ 2025J/src/main/java/frc/robot/commands/AlignToReefBasisVector.java
@@ -516,7 +516,7 @@ public class AlignToReefBasisVector extends Command {
             }
 
             // TODO: ask gkoch philip and eric
-            if(elapsedTime > 0.05 && isReefIntakeAlgaeSafe() && Superstructure.getInstance().getCurrentState() == SuperstructureState.STOW && destination == AlignmentDestination.MIDDLE) {
+            if(elapsedTime > 0.05 && isReefIntakeAlgaeSafe() && destination == AlignmentDestination.MIDDLE) {
                 Logger.getInstance().logEvent("Align to reef to reef intaking", true);
                 boolean high = Superstructure.getInstance().isHighAlgae();
                 Superstructure.getInstance().requestState(high ? SuperstructureState.REEF2_ALGAE_INTAKE : SuperstructureState.REEF1_ALGAE_INTAKE);
